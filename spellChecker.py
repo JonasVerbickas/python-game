36cf23f8978063d85f6e3530a4e5a49cf04b13bf
Jonas Verbickas <jonas.verbickas@student.manchester.ac.uk>
2020-10-29 19:32:55 +0000

    Add basic spellcheck (only finds errors)

--- spellChecker.py
+++ spellChecker.py
@@ -1,36 +1,71 @@
 def openFile(filename):
-	sentance = ""
+	text = ""
 	try:
 		f = open(filename, 'r')
-		sentance = f.readlines()
+		text = f.readlines()
 	except FileNotFoundError:
 		print("No such file found")
 		retype = input("Retype your file name or type in a number 0-2 to change mode")
 		if retype in ['0', '1', '2']:
-			mode = retype
+			return retype
 		else:
-			sentance = openFile(retype)
+			text = openFile(retype)
 
-	return sentance
+	return " ".join(text)
 
 
-def main():
-	mode = input("\n\n\n\nEnter mode ID:(0 to quit, 1 for input, 2 for file):")
+def getText():
+	mode = input("\n\n\n\n\n\n\n\n\n\n\n\nEnter mode ID(0 to quit, 1 for input, 2 for file):")
 
 	while mode not in ['0', '1', '2']:
 		print("WRONG ID!")
 		mode = input("Enter mode ID:(0 to quit, 1 for input, 2 for file):")
 
-
-	sentance = ""
-	while sentance == "":
+	text = ""
+	while text == "":
 		if mode == '0':
 			return 0
 		elif mode == '1':
-			sentance = input("Enter a sentance to spellcheck:")
+			text = input("Enter a text to spellcheck:")
 		elif mode == '2':
 			filename = input("Enter a filename of txt file for spellchecking:")
-			sentance = openFile(filename)
+			text = openFile(filename)
+			if text in ['0', '1', '2']:
+				mode = text
+	return text
+
+def formatText(text):
+	formattedText = text
+	for char in text:
+		if char.isalpha() == False:
+			formattedText = formattedText.replace(char, '')
+	return formattedText
+
+def main():
+	text = getText()
+	text = formatText(text)
+	print(text)
+
+	misspelled_count = 0
+	added_to_dict = 0
+	dictionary = open("EnglishWords.txt", 'r').read().split('\n')
+	for word_to_check in text:
+		for word_in_dict in dictionary:
+			if word_in_dict == word_to_check.lower() or len(word_to_check) == 0:
+				break;
+		else:
+			print(word_to_check + " not found")
+			option = 0
+			while option not in ['1', '2', '3', '4']:
+				option = input("1 to ignore, 2 to mark, 3 to add to dictionary, 4 to get a suggestion:")
+
+			if option == '1':
+				misspelled_count += 1
+			elif option == '3':
+				added_to_dict += 1
+
+
+	print("Misspelled words:" + str(misspelled_count))
 		
 
 main()
