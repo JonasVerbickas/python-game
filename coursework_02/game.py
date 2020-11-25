fc613a8b6c85eee429414c42caf99388f1a986a9
Jonas Verbickas <jonas.verbickas@student.manchester.ac.uk>
2020-11-25 01:12:20 +0000

    Add reticle

--- coursework_02/game.py
+++ coursework_02/game.py
@@ -1,38 +1,50 @@
 from tkinter import Tk, Canvas
 from time import sleep
-from math import sin, cos, atan, radians, degrees 
+from math import sin, cos, atan, radians, degrees, pi
 
 WIDTH = 700
 HEIGHT = 400
-STARTING_POINT = (20, HEIGHT)
 PLAYER_SIZE = 20
-SHURIKEN_SIZE = 5
+STARTING_POINT = (20, HEIGHT-PLAYER_SIZE)
+PROJECTILE_SIZE = 5
+RETICLE_SIZE = 50
 G = 0.0981
 
 PROJECTILES = []
 
+def getPlayerCenter():
+	player_coords = sky.coords(player)
+	# return point(x, y)
+	return (player_coords[0] + PLAYER_SIZE/2, player_coords[1]+PLAYER_SIZE/2)
+
 def calcAngle(mouse):
-	player_pos = sky.coords(player)
-	player_x = player_pos[2]
-	player_y = player_pos[1]
-	player2mouse = (mouse.x - player_x, mouse.y - player_y)
+	player_center = getPlayerCenter()
+	player2mouse = (mouse.x - player_center[0], mouse.y - player_center[1])
 	# restricts the area where projectiles can be thrown
 	if player2mouse[0] > 0 and player2mouse[1] < 0:
-		return abs(atan(player2mouse[0]/player2mouse[1]))
+		return pi/2 - abs(atan(player2mouse[0]/player2mouse[1]))
 	else:
 		# dont throw
 		return -1
 
 
+def aim(event):
+	player_center = getPlayerCenter()
+	angle = calcAngle(event)
+	if angle > 0:
+		xy = (player_center[0], player_center[1], player_center[0]+cos(angle)*RETICLE_SIZE, player_center[1]-sin(angle)*RETICLE_SIZE)
+		sky.coords(reticle, xy)
+
+
 def throw(event):
 	angle = calcAngle(event)
 	if angle > 0:
-		trajectory = [round(sin(angle), 2), round(cos(angle), 2)] # GET FROM MOUSE_POS LATER!!!
+		trajectory = [round(cos(angle), 2), round(sin(angle), 2)] # GET FROM MOUSE_POS LATER!!!
 		power = 6 # GET FROM THE TIME MOUSE WAS HELD DOWN FOR
 		vector = [direction * power for direction in trajectory]
 
-		player_coords = sky.coords(player)
-		xy = (player_coords[0]+PLAYER_SIZE/2, player_coords[1]-PLAYER_SIZE/2-SHURIKEN_SIZE, player_coords[0]+PLAYER_SIZE/2+SHURIKEN_SIZE, player_coords[1]-PLAYER_SIZE/2)
+		player_center = getPlayerCenter()
+		xy = (player_center[0]-PROJECTILE_SIZE/2, player_center[1]+PROJECTILE_SIZE/2, player_center[0]+PROJECTILE_SIZE/2, player_center[1]-PROJECTILE_SIZE/2)
 		projectile_id = sky.create_rectangle(xy, fill='red')
 		projectile = {"id": projectile_id, "vector": vector}
 		PROJECTILES.append(projectile)
@@ -40,33 +52,33 @@ def throw(event):
 
 def moveProjectiles():
 	for projectile in list(PROJECTILES):
+		# if not out of bounds
 		if sky.coords(projectile['id'])[3] < HEIGHT:
 			vector = projectile['vector']
 			sky.move(projectile['id'], vector[0], -vector[1])
 		else:
 			sky.delete(projectile['id'])
-			# ar galime remove kol esam for loope?? 
-			PROJECTILES.remove(projectile) # kazkodel veikia
+			PROJECTILES.remove(projectile)
 
 
 def applyGravity(): # rename later
 	for projectile in PROJECTILES:
 		projectile['vector'][1] -= G/2
 
+
+
 window = Tk()
 window.title("PEW PEW")
-
 sky = Canvas(width=WIDTH, height=HEIGHT, background='sky blue')
 sky.pack()
-
 ground = Canvas(width=WIDTH, height=HEIGHT/2, background='pale green')
 ground.pack()
 
-
 player = sky.create_oval(STARTING_POINT[0], STARTING_POINT[1], STARTING_POINT[0] + PLAYER_SIZE, STARTING_POINT[1] + PLAYER_SIZE, fill="tomato")
-#aim = sky.create_line(STARTING_POINT[0]+PLAYER_SIZE/1.2, STARTING_POINT[1], STARTING_POINT[0]+100, STARTING_POINT[1] - 75, fill='red')
+reticle = sky.create_line(0, 0, 0, 0, fill='red')
 
-sky.bind("<Button-1>", throw)
+sky.bind("<ButtonRelease-1>", throw)
+sky.bind("<Motion>", aim)
 
 while True:
 	moveProjectiles()
