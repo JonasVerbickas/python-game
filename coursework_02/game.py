21ea7e4f8eb1d1ccee9fb5b397f12d8c6a9b61a6
Jonas Verbickas <jonas.verbickas@student.manchester.ac.uk>
2020-11-25 00:28:19 +0000

    Implement basic aiming

--- coursework_02/game.py
+++ coursework_02/game.py
@@ -1,36 +1,52 @@
 from tkinter import Tk, Canvas
 from time import sleep
-from math import sin, cos, radians
+from math import sin, cos, atan, radians, degrees 
 
 WIDTH = 700
 HEIGHT = 400
-STARTING_POINT = (20, HEIGHT-50)
+STARTING_POINT = (20, HEIGHT)
 PLAYER_SIZE = 20
 SHURIKEN_SIZE = 5
 G = 0.0981
 
 PROJECTILES = []
 
-def throw(event):
-	trajectory = [round(sin(radians(60)), 2), round(cos(radians(60)), 2)] # GET FROM MOUSE_POS LATER!!!
-	power = 3 # GET FROM THE TIME MOUSE WAS HELD DOWN FOR
-	vector = [direction * power for direction in trajectory]
+def calcAngle(mouse):
+	player_pos = sky.coords(player)
+	player_x = player_pos[2]
+	player_y = player_pos[1]
+	player2mouse = (mouse.x - player_x, mouse.y - player_y)
+	# restricts the area where projectiles can be thrown
+	if player2mouse[0] > 0 and player2mouse[1] < 0:
+		return abs(atan(player2mouse[0]/player2mouse[1]))
+	else:
+		# dont throw
+		return -1
 
-	player_coords = sky.coords(player)
-	xy = (player_coords[2]+PLAYER_SIZE+SHURIKEN_SIZE, player_coords[1]-PLAYER_SIZE-SHURIKEN_SIZE, player_coords[2]+PLAYER_SIZE, player_coords[1]-PLAYER_SIZE)
-	projectile_id = sky.create_rectangle(xy, fill='red')
-	projectile = {"id": projectile_id, "vector": vector}
-	PROJECTILES.append(projectile)
+
+def throw(event):
+	angle = calcAngle(event)
+	if angle > 0:
+		trajectory = [round(sin(angle), 2), round(cos(angle), 2)] # GET FROM MOUSE_POS LATER!!!
+		power = 6 # GET FROM THE TIME MOUSE WAS HELD DOWN FOR
+		vector = [direction * power for direction in trajectory]
+
+		player_coords = sky.coords(player)
+		xy = (player_coords[0]+PLAYER_SIZE/2, player_coords[1]-PLAYER_SIZE/2-SHURIKEN_SIZE, player_coords[0]+PLAYER_SIZE/2+SHURIKEN_SIZE, player_coords[1]-PLAYER_SIZE/2)
+		projectile_id = sky.create_rectangle(xy, fill='red')
+		projectile = {"id": projectile_id, "vector": vector}
+		PROJECTILES.append(projectile)
 
 
 def moveProjectiles():
-	for projectile in PROJECTILES:
+	for projectile in list(PROJECTILES):
 		if sky.coords(projectile['id'])[3] < HEIGHT:
 			vector = projectile['vector']
 			sky.move(projectile['id'], vector[0], -vector[1])
 		else:
 			sky.delete(projectile['id'])
-			PROJECTILES.remove(projectile) # ar galime remove kol esam for loope?? 
+			# ar galime remove kol esam for loope?? 
+			PROJECTILES.remove(projectile) # kazkodel veikia
 
 
 def applyGravity(): # rename later
@@ -50,7 +66,7 @@ ground.pack()
 player = sky.create_oval(STARTING_POINT[0], STARTING_POINT[1], STARTING_POINT[0] + PLAYER_SIZE, STARTING_POINT[1] + PLAYER_SIZE, fill="tomato")
 #aim = sky.create_line(STARTING_POINT[0]+PLAYER_SIZE/1.2, STARTING_POINT[1], STARTING_POINT[0]+100, STARTING_POINT[1] - 75, fill='red')
 
-window.bind("<Button-1>", throw)
+sky.bind("<Button-1>", throw)
 
 while True:
 	moveProjectiles()
