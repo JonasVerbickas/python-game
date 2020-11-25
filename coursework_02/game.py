8a15eb396a14188b89c17b89216f90f6b4f601d5
Jonas Verbickas <jonas.verbickas@student.manchester.ac.uk>
2020-11-25 17:43:47 +0000

    Add basic HP system

--- coursework_02/game.py
+++ coursework_02/game.py
@@ -8,14 +8,15 @@ from random import randint
 WIDTH = 700
 HEIGHT = 400
 PLAYER_SIZE = 20
-STARTING_POINT = (20, HEIGHT-PLAYER_SIZE*1.75)
-WALL_DIMEN = (8, 12)
+STARTING_POINT = (40, HEIGHT-PLAYER_SIZE*1.75)
+WALL_DIMEN = (10, 12)
 
 PROJECTILES = []
 PROJECTILE_SIZE = 8
 POWER = 6
-MAX_SHRAPNEL_COUNT = 5
+MAX_SHRAPNEL_COUNT = 4
 SHRAPNEL = []
+SHRAPNEL_POWER_MULT = 0.5
 RETICLE_SIZE = 50
 G = 0.0981
 
@@ -26,6 +27,13 @@ TIME_BETWEEN_SPAWNS = 4000
 
 SCORE = 0
 SCORE_PER_Z = 100
+
+MAX_AMMO = 5
+CURRENT_AMMO = MAX_AMMO
+TIME_TO_RELOAD = 1000
+RELOAD_START_TIME = 0
+
+HP = 5
 
 def getCenter(coords):
 	x_mid = (coords[2] - coords[0])/2 + coords[0]
@@ -61,7 +69,6 @@ def coordsFromCenter(xy, size):
 def aim(event):
 	player_center = getCenter(sky.coords(player))
 	angle = angleBetweenPointAndPlayer([event.x, event.y])
-	print(angle)
 	if angle > 0:
 		xy = (player_center[0], player_center[1], player_center[0]+cos(angle)*RETICLE_SIZE, player_center[1]-sin(angle)*RETICLE_SIZE)
 		sky.coords(reticle, xy)
@@ -80,17 +87,22 @@ def shoot(event):
 	# thus being able to shoot when out of bounds
 	reticle_coords = sky.coords(reticle)
 	angle = angleBetweenPointAndPlayer([reticle_coords[2], reticle_coords[3]])
-	if angle > 0:
+	global CURRENT_AMMO
+	global RELOAD_START_TIME
+	if angle > 0 and CURRENT_AMMO > 0:
 		player_center = getCenter(sky.coords(player))
 		projectile = createProjectile(player_center, angle)
 		PROJECTILES.append(projectile)
+		CURRENT_AMMO -= 1
+		RELOAD_START_TIME = time()
 
 
 def createShrapnel(startintg_xy):
-	for i in range(randint(2, MAX_SHRAPNEL_COUNT)):
-		angle = randint(30, 100)
+	startintg_xy[1] = HEIGHT # this ensures that shrapnel always spawns
+	for i in range(MAX_SHRAPNEL_COUNT - randint(0, 1)): # randomize number of shrapnel
+		angle = randint(50, 100)
 		angle = radians(angle)
-		shrapnel = createProjectile(startintg_xy, angle, power=POWER/1.5)
+		shrapnel = createProjectile(startintg_xy, angle, power=POWER*SHRAPNEL_POWER_MULT)
 		SHRAPNEL.append(shrapnel)
 
 def BOOM(projectile, kill_id=-1, is_shrapnel=False):
@@ -160,25 +172,45 @@ def createZombie():
 
 
 def moveEnemies():
-	for z in ZOMBIES:
+	global HP
+	for z in list(ZOMBIES):
 		sky.move(z, -Z_SPEED, 0)
+		if sky.coords(z)[0] <= STARTING_POINT[0]:
+			HP -= 1
+			print(HP)
+			sky.delete(z)
+			ZOMBIES.remove(z)
 
 
 def updateText():
-	sky.itemconfig(txt, text="Score:" + str(SCORE))
+	sky.itemconfig(txt_score, text="Score:" + str(SCORE))
+	sky.itemconfig(txt_ammo, text="Ammo:" + str(CURRENT_AMMO))
+	sky.itemconfig(txt_health, text="Health:" + str(HP))
+
+
+def tryToReload():
+	global RELOAD_START_TIME
+	global CURRENT_AMMO
+	time_elapsed = (time() - RELOAD_START_TIME) * 1000
+	if time_elapsed > TIME_TO_RELOAD:
+		RELOAD_START_TIME = time()
+		CURRENT_AMMO += 1
 
 
 window = Tk()
 window.title("PEW PEW")
 sky = Canvas(width=WIDTH, height=HEIGHT, background='sky blue')
 sky.pack()
+
 ground = Canvas(width=WIDTH, height=HEIGHT/2, background='pale green')
 ground.pack()
 
-txt = sky.create_text(WIDTH/2, 20, text="Score: 0", font=("Arial", 20, 'bold'))
+txt_score = sky.create_text(WIDTH/2, 20, text="Score: 0", font=("Arial", 20, 'bold'))
+txt_ammo = sky.create_text(WIDTH/2, 50, text="Ammo: 0", font=("Arial", 20, 'bold'))
+txt_health = sky.create_text(WIDTH/2, 80, text="Health: 0", font=("Arial", 20, 'bold'))
 player = sky.create_oval(STARTING_POINT[0], STARTING_POINT[1], STARTING_POINT[0] + PLAYER_SIZE, STARTING_POINT[1] + PLAYER_SIZE, fill="tomato")
 reticle = sky.create_line(0, 0, 0, 0, fill='red')
-wall1 = sky.create_rectangle(60, HEIGHT, 60+WALL_DIMEN[0], HEIGHT-WALL_DIMEN[1], fill='brown4')
+wall1 = sky.create_rectangle(70, HEIGHT, 70+WALL_DIMEN[0], HEIGHT-WALL_DIMEN[1], fill='brown4')
 
 sky.bind("<ButtonRelease-1>", shoot)
 sky.bind("<Motion>", aim)
@@ -191,6 +223,8 @@ while True:
 		createZombie()
 		LAST_SPAWN_TIME = time()
 	moveEnemies()
+	if CURRENT_AMMO < MAX_AMMO:
+		tryToReload()
 	updateText()
 	window.update()
 	window.after(3)
