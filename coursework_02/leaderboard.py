fee80d9446138ba41d1533fd0386156960e9c5e2
Jonas Verbickas <jonas.verbickas@student.manchester.ac.uk>
2020-12-02 17:14:51 +0000

    Use grid for the leaderboard

--- coursework_02/leaderboard.py
+++ coursework_02/leaderboard.py
@@ -1,4 +1,5 @@
-from tkinter import Frame, Canvas, Button, NW
+from tkinter import Frame, Canvas, Button, Label #widgets
+from tkinter import W, E, NW, CENTER # anchors
 
 class LeaderBoard():
 	def getSortedListOfLeaders(self):
@@ -14,13 +15,11 @@ class LeaderBoard():
 			else:
 				list_of_leaders.append(key_val_split)
 		f.close()
-		return list_of_leaders
+		return list_of_leaders[:10] # only the TOP 9
 
 	def leaderList2String(self, leader_list):
 		output = ""
-		for i in range(len(leader_list)): # only the TOP 9
-			if i >= 9:
-				break
+		for i in range(len(leader_list)):
 			output += ("%d. %s\n") % (i+1, ":  ".join(leader_list[i]))
 		return output
 
@@ -36,12 +35,21 @@ class LeaderBoard():
 		canvas.pack()
 
 		canvas.create_image(0, 0, image=self.windowManager.menu_image, anchor=NW)
+		title = canvas.create_text(self.windowManager.getResolution()[0]/2, 60, text="LEADERBOARD", font=('Arial', 40, 'bold'))
 
 		back_button = Button(canvas, text="BACK", command=self.windowManager.menu, bg="sky blue", padx=30, pady=20)
 		back_button.place(relx=0.2, rely=0.15)
 
 		list_of_leaders = self.getSortedListOfLeaders()
-		string_of_leaders = self.leaderList2String(list_of_leaders)
-		title = canvas.create_text(self.windowManager.getResolution()[0]/2, 50, text="LEADERBOARD", font=('Arial', 32))
-		leaderboard_text = canvas.create_text(self.windowManager.getResolution()[0]/2, self.windowManager.getResolution()[1]/2, text=string_of_leaders, font=('Arial', 32))
+		print(list_of_leaders)
+		grid_of_leaders = Canvas(canvas, width=500, height=500)
+		grid_of_leaders.place(relx=0.5, rely=0.45, anchor=CENTER)
+		for leader_i in range(len(list_of_leaders)):
+			placed = Label(grid_of_leaders, text=str(leader_i+1), font=("Arial", 24))
+			placed.grid(row=leader_i, column=0, padx=5, sticky=E)
+			name = Label(grid_of_leaders, text=list_of_leaders[leader_i][0], font=("Arial", 24))
+			name.grid(row=leader_i, column=1, sticky=W)
+			score = Label(grid_of_leaders, text=list_of_leaders[leader_i][1], font=("Arial", 24))
+			score.grid(row=leader_i, column=2, padx=15)
+
 
